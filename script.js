f12521f9b9ed02dcd74824a326131c904c597933
VolotArtem <Flatout.93@mail.ru>
2022-12-15 17:23:12 +0300

    add listener for color mode after click, try to code refresh field

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const body = document.querySelector("body")
 const field = document.querySelector(".field")
-const oneBlock = 50; //test name for generate field
+const inputColor = document.querySelector(".color")
+const colorMode = document.querySelector(".color-mode")
+const squareUser = document.querySelector(".square")
+let oneBlock = 10; //test name for generate field
 const getFullField = (oneBlock) => { return square = oneBlock * oneBlock }//funct for calculate square field 
 //funct for add div in field grid РАЗДЕЛИ НА ДВЕ ФУНКЦИИ так как тут еще смена стиля для основного поля грид
 const paintFullField = (getFullField) => {
@@ -10,29 +13,43 @@ const paintFullField = (getFullField) => {
         let block = document.createElement("div");
         block.classList.add("element-field")
         field.append(block)
+
     }
 }
+//-------------остановился тут рефреш листа
+const clearFullField = () => {
+    let block = document.querySelectorAll(".element-field")
+
+}
+//-------------остановился тут рефреш листа
 paintFullField(getFullField(oneBlock)); // отрисовка главного холста(поля)
-let getBlockField = document.querySelectorAll(".element-field")//получаю нод лист всех созданый элементов на поле
+const getBlockField = document.querySelectorAll(".element-field")//получаю нод лист всех созданый элементов на поле
 //Функция покраски элемента(div block )
 const paint = (element) => {
     console.log(element)
     console.log(element.target)
-    element.target.style.backgroundColor = "red"
+    return element.target.style.backgroundColor = inputColor.value
 
 }
 function holdClick() {
     getBlockField.forEach((element) => element.addEventListener("mousemove", paint)) /*фукнция holdclick добавляет прослушиватель Движения мыши и 
     фызова фукции отрисовки*/
-    getBlockField.forEach((element) => element.addEventListener("mouseup", stopPaint))
+    getBlockField.forEach((element) => element.addEventListener("click", paint))
 }
 /*  фукнция stopPaint добавляет прослушиватель  Отжатого левого клика */
-let stopPaint = () => { getBlockField.forEach((element) => element.removeEventListener('mousemove', paint)) }
+const stopPaint = () => { getBlockField.forEach((element) => element.removeEventListener('mousemove', paint)) }
+
 body.addEventListener("mouseup", stopPaint)/*прослушиватель чтобы вне холста работали все мои прослушки, так удобней рисовать, иначе выйдя из
  холста и отжав там кнопку , фукнция рисования прилипнет к мышке и придется кликать еще раз*/
 body.addEventListener("mousedown", holdClick) // для выхода за холст с зажатой клавишей мыши и удобного возврата назад
 
+colorMode.addEventListener("click", paint)
 
+squareUser.addEventListener("click", function () {
+    oneBlock = prompt("Vvedite")
+    clearFullField();
+    paintFullField(getFullField(oneBlock))
+})
 
 
 
